da9c4cd90c5575a4e277d52ee4fde9a7f941c4ce
jitwxs <jitwxs@foxmail.com>
2020-04-21 22:52:42 +0800

    optimized: update grpc_demo proto utils

--- Java/grpc_demo/src/main/java/jit/wxs/grpc/common/ProtoUtils.java
+++ Java/grpc_demo/src/main/java/jit/wxs/grpc/common/ProtoUtils.java
@@ -1,21 +1,38 @@
 package jit.wxs.grpc.common;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.Message;
-import com.googlecode.protobuf.format.JsonFormat;
+import com.google.protobuf.StringValue;
+import com.google.protobuf.util.JsonFormat;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author jitwxs
  * @date 2020年04月05日 19:36
  */
 public class ProtoUtils {
-    private static JsonFormat JSON_FORMAT;
+    private static final Logger logger = Logger.getLogger(UserRpcServiceImpl.class.getName());
+
+    private static JsonFormat.TypeRegistry REGISTRY;
 
     static {
-        JSON_FORMAT = new JsonFormat();
+        REGISTRY = JsonFormat.TypeRegistry.newBuilder()
+                .add(StringValue.getDescriptor())
+                .build();
     }
 
     public static String toStr(Message message) {
-        return JSON_FORMAT.printToString(message);
+       try {
+           return JsonFormat.printer()
+                   .usingTypeRegistry(REGISTRY)
+                   .includingDefaultValueFields()
+                   .omittingInsignificantWhitespace()
+                   .print(message);
+       } catch (InvalidProtocolBufferException e) {
+           logger.log(Level.WARNING, "ProtoUtils#toStr error", e);
+       }
+       return "";
     }
-
 }
