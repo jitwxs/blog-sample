d9e85b38f0a3b0885784f64b05a7351c2a385ad9
jitwxs <jitwxs@foxmail.com>
2021-02-21 21:17:46 +0800

    es-sample standardized

---
+++ springboot-sample/es-sample/src/main/java/com/github/jitwxs/sample/es/EsApplication.java
@@ -0,0 +1,12 @@
+package com.github.jitwxs.sample.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsApplication.class, args);
+    }
+}
