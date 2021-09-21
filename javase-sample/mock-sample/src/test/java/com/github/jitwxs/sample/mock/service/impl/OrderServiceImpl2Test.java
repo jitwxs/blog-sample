ae8dc14ba3635581a5c4e961dbc1f93f74a464b7
jitwxs <jitwxs@foxmail.com>
2021-09-21 21:51:40 +0800

    update inject mock test

--- javase-sample/mock-sample/src/test/java/com/github/jitwxs/sample/mock/service/impl/OrderServiceImpl2Test.java
+++ javase-sample/mock-sample/src/test/java/com/github/jitwxs/sample/mock/service/impl/OrderServiceImpl2Test.java
@@ -5,12 +5,10 @@ import com.github.jitwxs.sample.mock.client.OrderClient;
 import com.github.jitwxs.sample.mock.domain.bean.Order;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.powermock.reflect.Whitebox;
 
 import java.math.BigDecimal;
@@ -18,18 +16,18 @@ import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(PowerMockRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class OrderServiceImpl2Test {
     @Spy
     private OrderServiceImpl orderService = new OrderServiceImpl();
     @Mock
     private OrderClient orderClient;
 
+    final Order order = DomainUtils.randomOrder();
+
     @Test
     public void testCalUserTotalUnDealAmount() {
-        final Order order = DomainUtils.randomOrder();
-
-        PowerMockito.when(orderClient.queryUserOrder(Mockito.anyLong(), Mockito.anyList())).thenReturn(Collections.singletonList(order));
+        Mockito.when(orderClient.queryUserOrder(Mockito.anyLong(), Mockito.anyList())).thenReturn(Collections.singletonList(order));
         Whitebox.setInternalState(orderService, "orderClient", orderClient);
 
         final BigDecimal amount = orderService.calUserTotalUnDealAmount(1L);
--- javase-sample/mock-sample/src/test/java/com/github/jitwxs/sample/mock/service/impl/OrderServiceImplTest.java
+++ javase-sample/mock-sample/src/test/java/com/github/jitwxs/sample/mock/service/impl/OrderServiceImplTest.java
@@ -8,26 +8,25 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
 import java.util.Collections;
 
 import static org.junit.Assert.*;
 
-@RunWith(PowerMockRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class OrderServiceImplTest {
     @InjectMocks
     private OrderServiceImpl orderService;
     @Mock
     private OrderClient orderClient;
 
+    final Order order = DomainUtils.randomOrder();
+
     @Test
     public void testCalUserTotalUnDealAmount() {
-        final Order order = DomainUtils.randomOrder();
-
-        PowerMockito.when(orderClient.queryUserOrder(Mockito.anyLong(), Mockito.anyList())).thenReturn(Collections.singletonList(order));
+        Mockito.when(orderClient.queryUserOrder(Mockito.anyLong(), Mockito.anyList())).thenReturn(Collections.singletonList(order));
 
         final BigDecimal amount = orderService.calUserTotalUnDealAmount(1L);
 
